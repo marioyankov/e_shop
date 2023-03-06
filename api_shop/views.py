3b0a286212bf9021abdda27f7e0c3579d7a14b2c
Mario Yankov <mario.iankov@gmail.com>
2023-03-06 16:45:43 +0200

    partially added view for product detail
    partially added view for product detail

--- api_shop/views.py
+++ api_shop/views.py
@@ -5,3 +5,14 @@ from django.views import View
 
 from rest_framework import views as rest_views, status, viewsets, permissions
 from rest_framework.response import Response
+from .models import Product
+
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    # product = get_object_or_404(Product, slug=slug)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'product_detail.html', context)
